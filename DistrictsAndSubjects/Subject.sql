8aa2db286071f028ea102a7d718bccaa7200b0a1
Супрун Сергей <4everyongg@mail.ru>
2021-11-17 14:16:39 +0400

    сделал скрипт на добавление элементов в таблицу, убрал плотность населения из subject

--- DistrictsAndSubjects/Subject.sql
+++ DistrictsAndSubjects/Subject.sql
@@ -3,9 +3,8 @@
 	[Code] bigint NOT NULL primary key,
 	[Name] nvarchar(50) NOT NULL,
 	[AdminCenterName] nvarchar(50) NOT NULL,
-	[Population] decimal(15,3) NOT NULL,
-	[Square] decimal(15,2) NOT NULL,
-	[PopulationDencity] decimal(15,3) NOT NULL,
+	[Population] real NOT NULL,
+	[Square] real NOT NULL,
 	[District] bigint NOT NULL 
 	foreign key references District(Code) 
 	on delete cascade
